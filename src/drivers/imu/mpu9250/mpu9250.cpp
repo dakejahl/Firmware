dabff81e498b7e1b984b666db3ae28bd6df8852a
Jake Dahl <jacob.dahl@tealdrones.com>
2018-05-11 14:59:41 -0600

    if use_i2c does not publish accel and gyro data

--- src/drivers/imu/mpu9250/mpu9250.cpp
+++ src/drivers/imu/mpu9250/mpu9250.cpp
@@ -1456,6 +1456,9 @@ MPU9250::measure()
 		_gyro->parent_poll_notify();
 	}
 
+#ifdef USE_I2C
+#else
+
 	if (accel_notify && !(_pub_blocked)) {
 		/* publish it */
 		orb_publish(ORB_ID(sensor_accel), _accel_topic, &arb);
@@ -1465,6 +1468,8 @@ MPU9250::measure()
 		/* publish it */
 		orb_publish(ORB_ID(sensor_gyro), _gyro->_gyro_topic, &grb);
 	}
+
+#endif
 
 	/* stop measuring */
 	perf_end(_sample_perf);
