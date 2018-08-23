961378c7b31218570bb40bcc4e7257788d69bf7f
Jake Dahl <jacob.dahl@tealdrones.com>
2018-08-23 17:20:14 -0600

    turned off USB mavlink stream. Added a check in our RC LOSS LOITER TIMEOUT failsafe for emergency battery status

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -2207,6 +2207,7 @@ Commander::run()
 		if (status.rc_signal_lost &&
 		    rc_loss_loiter_timeout > 0.0f &&
 		    (uint64_t)rc_loss_loiter_timeout <= (hrt_elapsed_time(&rc_signal_lost_timestamp) / 1000000ULL) &&
+		    _battery_warning != battery_status_s::BATTERY_WARNING_EMERGENCY &&
 		    internal_state.main_state != commander_state_s::MAIN_STATE_AUTO_RTL && armed.armed) {
 
 			if (TRANSITION_DENIED != main_state_transition(status, commander_state_s::MAIN_STATE_AUTO_RTL, status_flags, &internal_state)) {
